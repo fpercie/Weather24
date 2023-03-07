bec1280dca61b8343e618e5c2cb382ce58fc64f9
Fabio <fabioperestelo5@gmail.com>
2023-03-07 09:57:42 +0000

    update

---
+++ src/Title.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.scss";
+
+function Title(props: any) {
+  return (
+    <div className="parent">
+      <h1 className="title">Stockcheck24</h1>
+    </div>
+  );
+}
+
+export default Title;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const styleLoader = require("style-loader");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 8080,
+    open: true,
+  },
+};
