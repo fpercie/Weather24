c8465da7d335ff8242a7608090720c8ef03af233
Fabio <fabioperestelo5@gmail.com>
2023-03-17 13:57:39 +0000

    update

--- src/components/Popularstocks.tsx
+++ src/components/Popularstocks.tsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "/src/styles/Popularstocks.scss";
 
+const API_KEY = "cga2cuhr01qqlesgc7p0cga2cuhr01qqlesgc7pg";
+
 function Popularstocks(props: any) {
+  const [popularStocks, setPopularStocks] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch(`https://finnhub.io/api/v1/stock/popular?token=${API_KEY}&limit=5`)
+      .then((response) => response.json())
+      .then((data: any) => {
+        const symbols = data.map((stock: any) => stock.displaySymbol);
+        setPopularStocks(symbols);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="parentpopularstocks">
-      <button className="btn btn1"></button>
-      <button className="btn btn2"></button>
-      <button className="btn btn3"></button>
-      <button className="btn btn4"></button>
-      <button className="btn btn5"></button>
+      <button className="btn btn1">
+        <h1 className="popularstocktitle">{popularStocks[0]}</h1>
+      </button>
+      <button className="btn btn2">
+        <h1 className="popularstocktitle">{popularStocks[1]}</h1>
+      </button>
+      <button className="btn btn3">
+        <h1 className="popularstocktitle">{popularStocks[2]}</h1>
+      </button>
+      <button className="btn btn4">
+        <h1 className="popularstocktitle">{popularStocks[3]}</h1>
+      </button>
+      <button className="btn btn5">
+        <h1 className="popularstocktitle">{popularStocks[4]}</h1>
+      </button>
     </div>
   );
 }
