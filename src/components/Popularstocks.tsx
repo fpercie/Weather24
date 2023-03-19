b6fe4cecc77b1021466f48525ce0af675a4b6383
„Fabio“ <fabioperestelo5@gmail.com>
2023-03-19 20:58:56 +0000

    update

--- src/components/Popularstocks.tsx
+++ src/components/Popularstocks.tsx
@@ -1,9 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "/src/styles/Popularstocks.module.scss";
 
-const API_KEY = "cga2cuhr01qqlesgc7p0cga2cuhr01qqlesgc7pg";
+const API_KEY = "yUOn9GKpWCY7EbAGg_7KfZgrRV2BvjZk";
 
-function Popularstocks(props: any) {
+const date = new Date().toISOString().slice(0, 10);
+
+async function fetchPopularStocks(stock: string) {
+  const response = await fetch(
+    `https://api.polygon.io/v3/reference/tickers/${stock}?date=${date}&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data;
+}
+
+function Popularstocks() {
+  const [appleTitle, setAppleTitle] = useState(null);
+
+  useEffect(() => {
+    async function fetchTitles() {
+      const appleResponse = await fetchPopularStocks("AAPL");
+      setAppleTitle(appleResponse.name);
+    }
+    fetchTitles();
+  }, []);
+
   return (
     <div className={styles.parent}>
       <button className={styles.btn}>
@@ -19,7 +39,7 @@ function Popularstocks(props: any) {
         <h1 className={styles.popularstocktitle}></h1>
       </button>
       <button className={styles.btn}>
-        <h1 className={styles.popularstocktitle}></h1>
+        <h1 className={styles.popularstocktitle}>{appleTitle}</h1>
       </button>
     </div>
   );
