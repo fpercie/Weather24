024e81c16f56e9d7e989cb21902b6364323b9c8b
„Fabio“ <fabioperestelo5@gmail.com>
2023-03-19 12:11:31 +0000

    update

--- src/components/Popularstocks.tsx
+++ src/components/Popularstocks.tsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
-import "/src/styles/Popularstocks.module.scss";
+import styles from "/src/styles/Popularstocks.module.scss";
 
 const API_KEY = "cga2cuhr01qqlesgc7p0cga2cuhr01qqlesgc7pg";
 
 function Popularstocks(props: any) {
   return (
-    <div className="parentpopularstocks">
-      <button className="btn btn1">
-        <h1 className="popularstocktitle"></h1>
+    <div className={styles.parent}>
+      <button className={styles.btn}>
+        <h1 className={styles.popularstocktitle}></h1>
       </button>
-      <button className="btn btn2">
-        <h1 className="popularstocktitle"></h1>
+      <button className={styles.btn}>
+        <h1 className={styles.popularstocktitle}></h1>
       </button>
-      <button className="btn btn3">
-        <h1 className="popularstocktitle"></h1>
+      <button className={styles.btn}>
+        <h1 className={styles.popularstocktitle}></h1>
       </button>
-      <button className="btn btn4">
-        <h1 className="popularstocktitle"></h1>
+      <button className={styles.btn}>
+        <h1 className={styles.popularstocktitle}></h1>
       </button>
-      <button className="btn btn5">
-        <h1 className="popularstocktitle"></h1>
+      <button className={styles.btn}>
+        <h1 className={styles.popularstocktitle}></h1>
       </button>
     </div>
   );
