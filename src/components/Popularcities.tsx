1c4aefce6d1ece88260c057028359a011cae99f8
Fabio <fabioperestelo5@gmail.com>
2023-03-21 12:07:11 +0000

    update

--- src/components/Popularcities.tsx
+++ src/components/Popularcities.tsx
@@ -14,36 +14,60 @@ async function getWeather(city: string) {
 function Popularweather() {
   const [NewYorkTitle, setNewYorkTitle] = useState("");
   const [NewYorkCountry, setNewYorkCountry] = useState("");
+  const [NewYorkTemp, setNewYorkTemp] = useState("");
+  const [NewYorkIcon, setNewYorkIcon] = useState("");
+
   const [LondonTitle, setLondonTitle] = useState("");
   const [LondonCountry, setLondonCountry] = useState("");
+  const [LondonTemp, setLondonTemp] = useState("");
+  const [LondonIcon, setLondonIcon] = useState("");
+
   const [MadridTitle, setMadridTitle] = useState("");
   const [MadridCountry, setMadridCountry] = useState("");
+  const [MadridTemp, setMadridTemp] = useState("");
+  const [MadridIcon, setMadridIcon] = useState("");
+
   const [MunichTitle, setMunichTitle] = useState("");
   const [MunichCountry, setMunichCountry] = useState("");
+  const [MunichTemp, setMunichTemp] = useState("");
+  const [MunichIcon, setMunichIcon] = useState("");
+
   const [TokyoTitle, setTokyoTitle] = useState("");
   const [TokyoCountry, setTokyoCountry] = useState("");
+  const [TokyoTemp, setTokyoTemp] = useState("");
+  const [TokyoIcon, setTokyoIcon] = useState("");
 
   useEffect(() => {
     async function fetchTitles() {
       const weatherNewYork = await getWeather("New York");
       setNewYorkTitle(weatherNewYork.location.name);
       setNewYorkCountry(weatherNewYork.location.country);
+      setNewYorkTemp(weatherNewYork.current.temp_c);
+      setNewYorkIcon(weatherNewYork.current.condition.icon);
 
       const weatherLondon = await getWeather("London");
       setLondonTitle(weatherLondon.location.name);
       setLondonCountry(weatherLondon.location.country);
+      setLondonTemp(weatherLondon.current.temp_c);
+      setLondonIcon(weatherLondon.current.condition.icon);
 
       const weatherMadrid = await getWeather("Madrid");
       setMadridTitle(weatherMadrid.location.name);
       setMadridCountry(weatherMadrid.location.country);
+      setMadridTemp(weatherMadrid.current.temp_c);
+      setMadridIcon(weatherMadrid.current.condition.icon);
 
       const weatherMunich = await getWeather("Munich");
       setMunichTitle(weatherMunich.location.name);
       setMunichCountry(weatherMunich.location.country);
+      setMunichTemp(weatherMunich.current.temp_c);
+      setMunichIcon(weatherMunich.current.condition.icon);
 
       const weatherTokyo = await getWeather("Tokyo");
       setTokyoTitle(weatherTokyo.location.name);
       setTokyoCountry(weatherTokyo.location.country);
+      setTokyoTemp(weatherTokyo.current.temp_c);
+      setTokyoIcon(weatherTokyo.current.condition.icon);
     }
     fetchTitles();
   }, []);
@@ -54,26 +78,56 @@ function Popularweather() {
         <h1 className={styles.popularcitytitle}>
           {MadridTitle}, {MadridCountry}
         </h1>
+        <div className={styles.currenttempparent}>
+          <p className={styles.currenttemp}>
+            {Math.round(Number(MadridTemp))}°C
+          </p>
+          <img className={styles.currenttempicon} src={MadridIcon} />
+        </div>
       </button>
       <button className={styles.btn}>
         <h1 className={styles.popularcitytitle}>
           {MunichTitle}, {MunichCountry}
         </h1>
+        <div className={styles.currenttempparent}>
+          <p className={styles.currenttemp}>
+            {Math.round(Number(MunichTemp))}°C
+          </p>
+          <img className={styles.currenttempicon} src={MunichIcon} />
+        </div>
       </button>
       <button className={styles.btn}>
         <h1 className={styles.popularcitytitle}>
           {TokyoTitle}, {TokyoCountry}
         </h1>
+        <div className={styles.currenttempparent}>
+          <p className={styles.currenttemp}>
+            {Math.round(Number(TokyoTemp))}°C
+          </p>
+          <img className={styles.currenttempicon} src={TokyoIcon} />
+        </div>
       </button>
       <button className={styles.btn}>
         <h1 className={styles.popularcitytitle}>
           {LondonTitle}, {LondonCountry}
         </h1>
+        <div className={styles.currenttempparent}>
+          <p className={styles.currenttemp}>
+            {Math.round(Number(LondonTemp))}°C
+          </p>
+          <img className={styles.currenttempicon} src={LondonIcon} />
+        </div>
       </button>
       <button className={styles.btn}>
         <h1 className={styles.popularcitytitle}>
           {NewYorkTitle}, {NewYorkCountry}
         </h1>
+        <div className={styles.currenttempparent}>
+          <p className={styles.currenttemp}>
+            {Math.round(Number(NewYorkTemp))}°C
+          </p>
+          <img className={styles.currenttempicon} src={NewYorkIcon} />
+        </div>
       </button>
     </div>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import styles from "/src/styles/Title.module.scss";
-import {
-  faArrowTrendUp,
-  faCloudSun,
-  faSun,
-  faTemperature0,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const title = "Weather24";
 
@@ -14,7 +7,6 @@ function Title(props: any) {
   return (
     <div className={styles.parent}>
       <h1 className={styles.title}>{title}</h1>
-      <FontAwesomeIcon icon={faSun} className={styles.icon} />
     </div>
   );
 }
