a41d1f4ea169ab482183517f043e69f61e01137e
Fabio <fabioperestelo5@gmail.com>
2023-03-23 09:59:57 +0000

    update

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import styles from "/src/styles/Details.module.scss";
 
 function Details(props: any) {
-  return <div className={styles.parent}></div>;
+  return (
+    <div className={styles.parent}>
+      <div className={styles.child}></div>
+    </div>
+  );
 }
 
 export default Details;
